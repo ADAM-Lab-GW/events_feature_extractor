5db4088bae38878bcfd33413d37f909a45c926c2
Zahin Ahmed <zahinahmed97@gmail.com>
2026-03-16 05:24:34 -0400

    reverting changes to repeat augmentations

--- method/simclr.py
+++ method/simclr.py
@@ -90,9 +90,7 @@ class SimCLR(torch.nn.Module):
         for i, (_, data_augmented, _) in enumerate(train_loader):
             data = torch.stack(data_augmented, dim=1)
             d = data.size()
-
-            B, K, C, H, W = data.shape
-            train_x = data.view(B * K, C, H, W).cuda()
+            train_x = data.view(d[0]*2, d[2], d[3], d[4]).cuda()
             
             self.optimizer.zero_grad()
             if self.net.feature_extractor.name == "scnn":
--- train_feature_extractor.py
+++ train_feature_extractor.py
@@ -97,7 +97,7 @@ elif settings.method_name == "simclr":
                                                      data_type="simclr",
                                                      data_size=settings.batch_size,
                                                      train_transform=train_transform,
-                                                     repeat_augmentations=5,
+                                                     repeat_augmentations=2,
                                                      num_workers=settings.num_workers,
                                                      drop_last=False,
                                                      events_representation=settings.events_representation,
